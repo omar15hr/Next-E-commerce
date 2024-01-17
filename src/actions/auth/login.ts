02e26f96dfe85bf05482bbfd0b54e0fb2899db12
Omar Hernandez <omarhrn@gmail.com>
2024-01-17 12:21:24 -0300

    autenticacion lista

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -21,3 +21,24 @@ export async function authenticate(
     return 'CredentialsSingin';
   }
 }
+
+export const login = async( email: string, password: string ) => {
+
+  try {
+    
+    await signIn('credentials', { email, password })
+
+    return {
+      ok: true
+    };
+
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ok: false,
+      message: 'No se pudo iniciar sesión'
+    }
+    
+  }
+}
