aa02dd949601d9cfddb7bfc6c11e5b74f9bf2566
Omar Hernandez <omarhrn@gmail.com>
2024-01-18 10:55:10 -0300

    Guardar direccion del cliente

--- src/app/(shop)/checkout/address/ui/AddressForm.tsx
+++ src/app/(shop)/checkout/address/ui/AddressForm.tsx
@@ -6,6 +6,8 @@ import { useForm } from "react-hook-form";
 import { Country } from "@/interfaces";
 import { useAddressStore } from "@/store";
 import { useEffect } from "react";
+import { setUserAddress } from "@/actions";
+import { useSession } from "next-auth/react";
 
 type FormInputs = {
   firstName: string;
@@ -31,8 +33,14 @@ export function AddressForm({ countries }: Props) {
     }
   });
 
+  const { data: session } = useSession({
+    required: true,
+  });
+
   const setAddress = useAddressStore( state => state.setAddress );
   const address = useAddressStore( state => state.address );
+
+
 
   useEffect(() => {
     if ( address.firstName ) {
@@ -41,9 +49,16 @@ export function AddressForm({ countries }: Props) {
   },[])
 
   const onSubmit = ( data: FormInputs ) => {
-    console.log({data});
 
     setAddress(data);
+    const { rememberAddress, ...restAddress } = data;
+
+    if ( data.rememberAddress ) {
+      setUserAddress(restAddress, session!.user.id);
+
+    } else {
+
+    }
   }
 
   return (
